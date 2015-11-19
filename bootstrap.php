ca5fcb4c7f577afee03233d1d262c94328ec7af3
Victor <victor.miletskiy@gmail.com>
2015-11-19 17:50:56 +0200

    Config removed

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,6 @@ Author URI: http://govright.org/
 License: MIT
 */
 
-require_once(__DIR__ . '/includes/config.php');
 require_once(__DIR__ . '/includes/utils.php');
 require_once(__DIR__ . '/includes/interceptors/auth.php');
 require_once(__DIR__ . '/includes/interceptors/social.php');
