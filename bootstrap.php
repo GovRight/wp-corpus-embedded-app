fccee72cd6705b89b88ed3d9b5899a6b73b6c7c2
Victor <victor.miletskiy@gmail.com>
2015-11-29 20:17:20 +0200

    Extended shortcode attributes

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
 Plugin Name: GovRight Corpus Embedded App
 Plugin URI: https://github.com/GovRight/wp-corpus-embedded-app
 Description: Plugin to help you to embed a GovRight application into your site. Provides easy app setup and automates some things like authentication callback and social sharing.
-Version: 1.0.2
+Version: 1.1.0
 Author: GovRight
 Author URI: http://govright.org/
 License: MIT
--- includes/utils.php
+++ includes/utils.php
@@ -27,24 +27,45 @@ function wpce_apps() {
 /**
  * Get app loader html
  */
-function wpce_get_loader() {
+function wpce_get_loader($atts = array()) {
     $loader_url = wpce_config('loader_url');
     if(empty($loader_url)) {
         $app_name = wpce_config('name');
         if(empty($app_name)) {
             $message = 'GovRight embedded app is not selected. Select an application in the plugin settings.';
         } else {
-            $message = 'Can\'t load the ' . $app_name . ' application, app config is invalid.';
+            $message = 'Can\'t load the ' . $app_name . ' application, missing loader url.';
         }
         return '<script>console.warn("' . $message . '");</script>';
     }
     $law_slug = get_option('wpce_law_slug');
-    return '<script src="' . $loader_url . '" data-law-slug="' . $law_slug . '"></script>';
+    if(!is_array($atts)) {
+        $atts = array();
+    }
+    if(!isset($atts['data-law-slug'])) {
+        $atts['data-law-slug'] = $law_slug;
+    }
+
+    return '<script src="' . $loader_url . '" ' . wpce_atts_string($atts) . '"></script>';
+}
+
+/**
+ * Convert shortcode attributes array to string
+ */
+function wpce_atts_string($atts) {
+    if(empty($atts)) {
+        return '';
+    }
+    $string = '';
+    foreach ($atts as $key => $val) {
+        $string .= $key . '="' . $val . '" ';
+    }
+    return $string;
 }
 
 /**
  * Print app loader
  */
-function corpus_app() {
-    echo wpce_get_loader();
+function corpus_app($atts = array()) {
+    echo wpce_get_loader($atts);
 }
