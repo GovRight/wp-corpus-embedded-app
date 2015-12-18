de8d8e9a18d20ba9e58ca3b14f328fb0d81a2825
Victor <victor.miletskiy@gmail.com>
2015-12-18 16:13:33 +0200

    Revtracker production url

--- includes/config.php
+++ includes/config.php
@@ -2,9 +2,9 @@
 $WPCE_CONFIG = array(
     'rev-tracker' => array(
         'name' => 'Revision Tracker',
-        'auth_post_url' => 'http://revtracker/auth/success',
-        'social_url' => 'http://revtracker',
-        'loader_url' => 'http://retracker/loader.js',
+        'auth_post_url' => 'http://revtracker.legislationlab.org/auth/success',
+        'social_url' => 'http://revtracker.legislationlab.org/social',
+        'loader_url' => 'http://revtracker.legislationlab.org/loader.js',
         'law_route' => '/law/'
     ),
     'impact-project' => array(
